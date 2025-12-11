92bd19e69c6e99709d3af3f320f052e200b0cd07
illia-fz <77698519+illia-fz@users.noreply.github.com>
2025-12-11 16:10:39 -0500

    Refactor: Minor code style improvement in compound.py (Commit 5)

--- src/compound.py
+++ src/compound.py
@@ -13,3 +13,4 @@ if __name__ == "__main__":
 # Commit 2: Adding a comment to compound.py
 # Commit 3: Adding a comment to compound.py
 # Commit 4: Adding a comment to compound.py
+# Commit 5: Adding a comment to compound.py
